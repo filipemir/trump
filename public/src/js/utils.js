a02b428ce2da77206c5900c821e6f107a865b0e5
Filipe Miranda <fmiranda@fmiranda-MBP.local>
2017-01-02 16:03:41 -0500

    Update isEmoji

--- public/src/js/utils.js
+++ public/src/js/utils.js
@@ -1,10 +1,6 @@
 function isEmoji(str) {
-  const ranges = [
-    '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
-    '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
-    '\ud83d[\ude80-\udeff]'  // U+1F680 to U+1F6FF
-  ];
-  return str && str.match && str.match(ranges.join('|'));
+  const regEx = /\ud83c[\udf00-\udfff]|\ud83d[\udc00-\ude4f]|\ud83d[\ude80-\udeff]/;
+  return regEx.test(str);
 }
 
 export { isEmoji };
