2375b29022123d1f916867a33a827d371ccd1d8b
Filipe <filipe.m.mir@gmail.com>
2017-06-17 05:28:39 -0400

    Now runs in local development and docker-compose

--- app/config.js
+++ app/config.js
@@ -4,7 +4,6 @@ module.exports = {
   db: {
     seeder: `${paths.appDir}/trumpisms.json`,
     production: {
-      location: process.env.DB_LOCATION,
       URI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_LOCATION}/the-best-words`,
       db: 'the-best-words',
       collection: 'quotes',
@@ -12,7 +11,9 @@ module.exports = {
       pw: process.env.DB_PASSWORD
     },
     development: {
-      location: 'localhost',
+      // For the URI below to work in local development,
+      // ensure that the following line is in the hosts
+      // file: 127.0.0.1    mongo
       URI: 'mongodb://mongo/the-best-words',
       db: 'the-best-words',
       collection: 'quotes',
